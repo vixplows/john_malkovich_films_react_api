91694a8880b9ddf6f00182ec2d11395e1e2f3341
vixplows <vicky@vickyplows.co.uk>
2017-08-22 16:29:08 +0100

    Initial commit

---
+++ client/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.onload = function(){
+  ReactDOM.render(
+    <h1> Films where John Malkovich Acts or Directs</h1>,
+    document.getElementById('app')
+  );
+}
